cc43a60a9b147728c0a092ec0ee1b32647f49149
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2024-12-25 17:22:58 +0100

    svelte correction & layout styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,23 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<section class="layout">
+	{@render children()}
+</section>
+
+<style>
+	:global(body) {
+		height: 100vh;
+		margin: 0;
+		padding: 0;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.layout {
+	  flex: 1;
+	  background-color: #1a1a1a;
+	  padding: 2rem;
+	  color: aliceblue;
+	}
+  </style>
--- src/routes/gameboard/+page.svelte
+++ src/routes/gameboard/+page.svelte
@@ -2,7 +2,7 @@
 	import GameBoard from "$lib/components/GameBoard.svelte";
 </script>
 
-<main>
+<main class="">
 	<h1 class="text-center w-ful p-4">Battleship</h1>
 	<GameBoard />
 </main>
